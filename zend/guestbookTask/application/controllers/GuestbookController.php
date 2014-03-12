b99f900c2491e61dacabef3214626ab9b6bb89ee
Misha <shooterm@mail.ru>
2014-03-12 21:45:46 +0200

    Added guestbook task to my repository

---
+++ zend/guestbookTask/application/controllers/GuestbookController.php
@@ -0,0 +1,25 @@
+<?php
+
+class GuestbookController extends Zend_Controller_Action
+{
+	public function indexAction()
+	{
+		$guestbook = new Application_Model_Guestbook();
+		$this->view->entries = $guestbook->fetchAll();
+	}
+	
+	public function signAction()
+    {
+        $request = $this->getRequest();
+        $form    = new Application_Form_Guestbook();
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $model = new Application_Model_Guestbook($form->getValues());
+                $model->save();
+                return $this->_helper->redirector('index');
+            }
+        }
+        $this->view->form = $form;
+    }
+	
+}
